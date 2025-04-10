ac5e9c29f6323aa705506c651ecd5ee698713e0e
Mister D. Ace <ode808prod@gmail.com>
2025-04-10 09:34:36 +0200

    added the filter method

---
+++ filter.js
@@ -0,0 +1,21 @@
+Array.prototype.myFilter = function(callback) {
+  const result = [];
+
+  for (let i = 0; i < this.length; i++) {
+    const item = this[i];
+    if (callback(item, i, this)) {
+      result.push(item);
+    }
+  }
+
+  return result;
+};
+
+const arr = [7, 10, 17, 14]
+const evenNum = arr.myFilter(num=>{
+  if(num%2===0){
+    return num
+  }
+})
+
+console.log(evenNum)
